e8c83a87f96d8d0801fc2a101346489f5fdd2e1a
link00000000 <crandall.logan@gmail.com>
2021-04-21 01:25:33 -0400

    Add colored messages to install script

--- .bin/install.sh
+++ .bin/install.sh
@@ -8,24 +8,53 @@ echo ' | (_| | (_) | |_| | | | |  __/\__ \ '
 echo '  \__,_|\___/ \__|_| |_|_|\___||___/ '
 echo '                                     '
 
-git clone --bare git@github.com:link00000000/dotfiles.git $HOME/.cfg
-
 config() {
     /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
 }
 
+color() {
+    ecode=$1
+    text=$2
+
+    printf "\033[${ecode}m${text}\033[0m"
+}
+
+red() {
+    text=$1
+
+    color '0;31' "$text"
+}
+
+cyan() {
+    text=$1
+    color '0;36' "$text"
+}
+
+light_green() {
+    text=$1
+    color '1;32' "$text"
+}
+
+echo $(cyan "Fetching dotfiles...")
+git clone --bare git@github.com:link00000000/dotfiles.git $HOME/.cfg
+echo
+
 config checkout > /dev/null 2>&1
 if [ $? != 0 ]; then
-    echo "Backing up pre-existing dot files..."
+    echo $(red "Pre-existing dofiles found!")
+    echo "Backing up pre-existing dotfiles..."
 
     for file in $(config checkout 2>&1 | egrep "\s+\." | awk {'print $1'}); do
         mkdir --parents .config-backup/$(dirname $file)
         mv $file .config-backup/$file
     done
 
-    echo "Dotfiles backed up to $HOME/.config-backup"
+    echo $(cyan "Dotfiles backed up to $HOME/.config-backup")
 fi
+echo
 
 config checkout
 config config status.showUntrackedFiles no
+
+echo $(light_green "Done!")
 
