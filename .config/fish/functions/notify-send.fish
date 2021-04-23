632d73bb35650670f1ee1f651df6d92886e57fb8
link00000000 <crandall.logan@gmail.com>
2021-04-23 13:29:19 -0400

    Add message to notify-send function if .exe is not available

--- .config/fish/functions/notify-send.fish
+++ .config/fish/functions/notify-send.fish
@@ -1,4 +1,9 @@
 # Defined via `source`
 function notify-send --wraps=notify-send.exe --description 'alias notify-send=notify-send.exe'
-  notify-send.exe $argv; 
+  if type -q notify-send.exe
+    notify-send.exe $argv; 
+  else
+    echo "notify-send.exe must be installed on and available in the path of the Windows host"
+    echo "GitHub Repository: https://github.com/vaskovsky/notify-send"
+  end
 end
