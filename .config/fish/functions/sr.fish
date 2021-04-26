65f737162353033e2d8a966ab0f12d22db4e6df2
link00000000 <crandall.logan@gmail.com>
2021-04-26 17:40:56 -0400

    Open surfraw with brave on windows host

---
+++ .config/fish/functions/sr.fish
@@ -0,0 +1,4 @@
+# Defined in - @ line 1
+function sr --wraps='BROWSER=/mnt/c/Users/logan/AppData/Local/BraveSoftware/Brave-Browser/Application/brave.exe surfraw' --description 'alias sr=BROWSER=/mnt/c/Users/logan/AppData/Local/BraveSoftware/Brave-Browser/Application/brave.exe surfraw'
+  BROWSER=/mnt/c/Users/logan/AppData/Local/BraveSoftware/Brave-Browser/Application/brave.exe surfraw $argv;
+end
---
+++ .config/fish/functions/surfraw.fish
@@ -0,0 +1,4 @@
+# Defined in - @ line 1
+function surfraw --wraps='BROWSER=/mnt/c/Users/logan/AppData/Local/BraveSoftware/Brave-Browser/Application/brave.exe surfraw' --wraps='BROWSER=/mnt/c/Users/logan/AppData/Local/BraveSoftware/Brave-Browser/Application/brave.exe command surfraw' --description 'alias surfraw=BROWSER=/mnt/c/Users/logan/AppData/Local/BraveSoftware/Brave-Browser/Application/brave.exe command surfraw'
+  BROWSER=/mnt/c/Users/logan/AppData/Local/BraveSoftware/Brave-Browser/Application/brave.exe command surfraw $argv;
+end
