4bf6e55ae63998c55345d13f3d85c65e6cea7751
Logan Crandall <logan@accidentallycoded.com>
2023-09-17 14:39:15 -0400

    Change shell from pwsh to nu

--- vim/nvim/lua/configs/local/LOGAN-DESKTOP.lua
+++ vim/nvim/lua/configs/local/LOGAN-DESKTOP.lua
@@ -3,12 +3,12 @@ local shell = require('utils.shell')
 local M = {}
 
 M.setup = function ()
-    shell.set('pwsh.exe', {
+    shell.set('nu.exe', {
         shell_x_quote = '',
-        shell_cmd_flag = '-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command ',
+        shell_cmd_flag = '--commands ',
         shell_quote = '',
-        shell_pipe = '| Out-File -Encoding UTF8 %s',
-        shell_redir = '| Out-File -Encoding UTF8 %s',
+        shell_pipe = '',
+        shell_redir = '',
     })
 end
 
