1ec4e13f2d6eadcf471c0169957a7db4184f9732
Logan Crandall <logan@accidentallycoded.com>
2024-02-24 15:36:24 -0500

    Disable underlines from indent blankline

--- vim/nvim/lua/plugins/indent-blankline.lua
+++ vim/nvim/lua/plugins/indent-blankline.lua
@@ -1,6 +1,11 @@
 -- The config function must be specified for this plugin because lazy.nvim cannot figure out which file to require to call setup
 local function config ()
-    require("ibl").setup()
+    require("ibl").setup({
+        scope = {
+            show_start = false,
+            show_end = false,
+        }
+    })
 end
 
 ---@type PluginModule
