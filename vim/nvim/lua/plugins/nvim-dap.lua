5386e4615e527ad57dbd909d14dfa52d8e67c4db
Logan Crandall <logan@accidentallycoded.com>
2024-04-13 15:28:14 -0400

    Setup dap icons and highlights

--- vim/nvim/lua/plugins/nvim-dap.lua
+++ vim/nvim/lua/plugins/nvim-dap.lua
@@ -1,9 +1,40 @@
 ---@type PluginModule
-return 
+return
 {
     spec = {
         "mfussenegger/nvim-dap",
         lazy = true,
-        event = { "BufEnter" },
+        config = function ()
+            local codicons = require("codicons")
+
+            vim.fn.sign_define("DapBreakpoint", {
+                text = codicons.get("circle-filled", "icon"),
+                texthl = "Error",
+            })
+
+            vim.fn.sign_define("DapBreakpointCondition", {
+                text = codicons.get("debug-breakpoint-conditional", "icon"),
+                texthl = "Error",
+            })
+
+            vim.fn.sign_define("DapLogPoint", {
+                text = codicons.get("debug-breakpoint-log", "icon"),
+                texthl = "Error",
+            })
+
+            vim.fn.sign_define("DapStopped", {
+                text = codicons.get("arrow-right", "icon"),
+                texthl = "WarningMsg",
+                linehl = "Search",
+            })
+
+            vim.fn.sign_define("DapRejected", {
+                text = codicons.get("circle-filled", "icon"),
+                texthl = "BufferOffset",
+            })
+        end,
+        dependencies = {
+            require("plugins.codicons").spec,
+        }
     }
 }
