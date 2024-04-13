0ad0d1565d4826f5cb49cfb23c9ec3a620fecb71
Logan Crandall <logan@accidentallycoded.com>
2024-04-13 15:27:09 -0400

    Make treesitter not lazy and auto-enable in buffers

--- vim/nvim/lua/plugins/nvim-treesitter.lua
+++ vim/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,89 +1,55 @@
 -- Requires a C compiler. On Windows `scoop install zig`
 -- Some languages require tree-sitter-cli `npm i -g tree-sitter-cli`
 
-local M = {}
+---@type PlugiNModule
+return {
+    spec = {
+        'nvim-treesitter/nvim-treesitter',
+        build = ':TSUpdate',
+        lazy = false,
+        config = function ()
+            require("nvim-treesitter.install").compilers = { 'zig', vim.fn.getenv('CC'), 'cc', 'gcc', 'clang', 'cl' }
+            require("nvim-treesitter.configs").setup({
+                ensure_installed = {
+                    "bash",
+                    "c",
+                    "c_sharp",
+                    "cmake",
+                    "comment",
+                    "cpp",
+                    "css",
+                    "dockerfile",
+                    "gitcommit",
+                    "gitignore",
+                    "html",
+                    "javascript",
+                    "jsdoc",
+                    "json",
+                    "json5",
+                    "jsonc",
+                    "lua",
+                    "make",
+                    "markdown",
+                    "markdown_inline",
+                    "python",
+                    "regex",
+                    "scss",
+                    "sql",
+                    "toml",
+                    "tsx",
+                    "typescript",
+                    "vim",
+                    "yaml"
+                },
 
-local function config ()
-    local treesitter = require('nvim-treesitter.configs')
-    local treesitter_install = require('nvim-treesitter.install')
+                -- Automatically install missing parsers when entering buffer
+                auto_install = true,
 
-    treesitter_install.compilers = { 'zig', vim.fn.getenv('CC'), 'cc', 'gcc', 'clang', 'cl' }
-
-    treesitter.setup({
-        -- A list of parser names, or "all"
-        ensure_installed = {
-            "bash",
-            "c",
-            "c_sharp",
-            "cmake",
-            "comment",
-            "cpp",
-            "css",
-            "dockerfile",
-            "gitcommit",
-            "gitignore",
-            "html",
-            "javascript",
-            "jsdoc",
-            "json",
-            "json5",
-            "jsonc",
-            "lua",
-            "make",
-            "markdown",
-            "markdown_inline",
-            "python",
-            "regex",
-            "scss",
-            "sql",
-            "toml",
-            "tsx",
-            "typescript",
-            "vim",
-            "yaml"
-        },
-
-        -- Automatically install missing parsers when entering buffer
-        auto_install = true,
-
-        highlight = {
-            -- Disable for large files
-            disable = function(lang, buf)
-                local max_filesize = 100 * 1024 -- 100 KB
-                local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-                if ok and stats and stats.size > max_filesize then
-                    return true
-                end
-            end,
-        },
-    })
-end
-
-M.spec = {
-    'nvim-treesitter/nvim-treesitter',
-    build = ':TSUpdate',
-    lazy = true,
-    config = config,
-    event = { 'BufEnter' },
-    cmd = {
-        'TSBufToggle',
-        'TSEnable',
-        'TSBufEnable',
-        'TSEditQuery',
-        'TSDisable',
-        'TSUpdate',
-        'TSToggle',
-        'TSInstall',
-        'TSUninstall',
-        'TSBufDisable',
-        'TSConfiglnfo',
-        'TSModulelnfo',
-        'TSUpdateSync',
-        'TSInstalllnfo',
-        'TSInstallSync',
-        'TSEditQueryUserAfter',
-        'TSInstaIIFrornGramar',
+                highlight = {
+                    enable = true,
+                    additional_vim_regex_highlighting = false,
+                },
+            })
+        end,
     }
 }
-
-return M
