b7eeb792f164f6b7e03ed57de0f1bfe04d7f0515
Logan Crandall <logan@accidentallycoded.com>
2024-08-29 20:30:21 -0400

    Open current file in mini-file

--- vim/nvim-lean/lua/plugins/mini-files.lua
+++ vim/nvim-lean/lua/plugins/mini-files.lua
@@ -2,13 +2,9 @@ return {
   "echasnovski/mini.files",
   version = "*",
   keys = {
-    { "<Leader>f", function () require("mini.files").open() end, desc = "Open mini file picker" },
+    { "<Leader>f", function () require("mini.files").open(vim.api.nvim_buf_get_name(0)) end, desc = "Open mini file picker" },
   },
   config = function ()
-    require("mini.files").setup {
-      mappings = {
-        syncronize = "<CR>",
-      }
-    }
+    require("mini.files").setup {}
   end
 }
