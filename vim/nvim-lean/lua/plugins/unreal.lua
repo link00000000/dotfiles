b14e4c14874042ad0094e73d927ef99c4e009f3b
Logan Crandall <logan@accidentallycoded.com>
2024-09-01 15:07:21 -0400

    Add unreal neovim plugin

---
+++ vim/nvim-lean/lua/plugins/unreal.lua
@@ -0,0 +1,12 @@
+function is_unreal_project ()
+  -- TODO: Check for UnrealNvim.json or *.uproject
+end
+
+return {
+  "zadirion/Unreal.nvim",
+  dependencies = {
+    "tpope/vim-dispatch"
+  },
+  ft = { "cpp", "cxx", "h", "hpp" },
+  cmd = { "UnrealGenWithEngine", "UnrealGen", "UnrealBuild", "UnrealRun", "UnrealCD" },
+}
