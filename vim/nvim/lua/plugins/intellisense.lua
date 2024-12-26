577f2d6e155f41e36f97aeac60c570be8c1a47e0
Logan Crandall <logan@accidentallycoded.com>
2024-12-26 12:16:31 -0500

    Disable luasnip plugin

--- vim/nvim/lua/plugins/intellisense.lua
+++ vim/nvim/lua/plugins/intellisense.lua
@@ -8,8 +8,8 @@ return {
       "hrsh7th/cmp-path", 
       "hrsh7th/cmp-nvim-lsp", 
       "hrsh7th/cmp-nvim-lsp-signature-help",
-      "saadparwaiz1/cmp_luasnip",
-      { "l3mon4d3/luasnip", version = "v2.*", build = "make install_jsregexp" },
+      --"saadparwaiz1/cmp_luasnip",
+      --{ "l3mon4d3/luasnip", version = "v2.*", build = "make install_jsregexp" },
     },
     config = function ()
       local cmp = require("cmp")
@@ -17,11 +17,11 @@ return {
       cmp.setup {
         preselect = "none",
 
-        snippet = {
-          expand = function (args)
-            require("luasnip").lsp_expand(args.body)
-          end
-        },
+        --snippet = {
+        --  expand = function (args)
+        --    require("luasnip").lsp_expand(args.body)
+        --  end
+        --},
 
         -- Code editing
         mapping = {
@@ -45,7 +45,7 @@ return {
         },
 
         sources = cmp.config.sources({
-          { name = "luasnip" },
+          --{ name = "luasnip" },
           { name = "nvim_lsp_signature_help" },
           { name = "nvim_lsp" },
         }, {
