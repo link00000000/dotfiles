96d063b5ab2db4aa3e86cb807994a20a53ad16f6
link00000000 <crandall.logan@gmail.com>
2025-01-26 19:35:10 -0500

    Setup custom injections for golang c embedding

--- vim/nvim/after/queries/go/injections.scm
+++ vim/nvim/after/queries/go/injections.scm
@@ -1,10 +1,51 @@
 ;;extends
 
+((comment) @injection.content
+  (#set! injection.language "comment"))
+
+(call_expression
+  (selector_expression) @_function
+  (#any-of? @_function
+    "regexp.Match" "regexp.MatchReader" "regexp.MatchString" "regexp.Compile" "regexp.CompilePOSIX"
+    "regexp.MustCompile" "regexp.MustCompilePOSIX")
+  (argument_list
+    .
+    [
+      (raw_string_literal)
+      (interpreted_string_literal)
+    ] @injection.content
+    (#offset! @injection.content 0 1 0 -1)
+    (#set! injection.language "regex")))
+
+((comment) @injection.content
+  (#match? @injection.content "/\\*!([a-zA-Z]+:)?re2c")
+  (#set! injection.language "re2c"))
+
+((call_expression
+  function: (selector_expression
+    field: (field_identifier) @_method)
+  arguments: (argument_list
+    .
+    (interpreted_string_literal) @injection.content))
+  (#any-of? @_method "Printf" "Sprintf" "Fatalf" "Scanf" "Errorf" "Skipf" "Logf")
+  (#set! injection.language "printf"))
+
+((call_expression
+  function: (selector_expression
+    field: (field_identifier) @_method)
+  arguments: (argument_list
+    (_)
+    .
+    (interpreted_string_literal) @injection.content))
+  (#any-of? @_method "Fprintf" "Fscanf" "Appendf" "Sscanf")
+  (#set! injection.language "printf"))
+
 ((source_file
   (comment) @injection.content (#offset! @injection.content 1 0 -1 0)
   (import_declaration
     (import_spec
       path: (interpreted_string_literal) @_path (#eq? @_path "\"C\""))))
  (#set! injection.language "c")
+ (#set! injection.combine)
 )
 
