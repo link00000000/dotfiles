3e97a1ea8b51097bddec035f1993417c4260757c
Logan Crandall <logan@accidentallycoded.com>
2025-02-15 16:28:02 -0500

    Pin glance version because of regression introduced for nvim version 0.11

--- vim/nvim/lua/plugins/dap.lua
+++ vim/nvim/lua/plugins/dap.lua
@@ -101,7 +101,7 @@ return {
         local dap = require("dap")
         local dapui = require("dapui")
 
-        dapui.setup {}
+        dapui.setup{}
 
         dap.listeners.before.attach.dapui_config = function () dapui.open() end
         dap.listeners.before.launch.dapui_config = function () dapui.open() end
