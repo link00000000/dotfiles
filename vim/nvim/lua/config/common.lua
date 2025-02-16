5eed3a49efd4185361640537c4160fcb3c94c9a9
link00000000 <crandall.logan@gmail.com>
2025-02-15 21:13:48 -0500

    Fix Redir command not splitting output into lines

--- vim/nvim/lua/config/common.lua
+++ vim/nvim/lua/config/common.lua
@@ -113,7 +113,8 @@ vim.api.nvim_create_user_command("ExecuteBuffer", function (args)
 end, { range = true })
 
 vim.api.nvim_create_user_command('Redir', function(ctx)
-  local lines = vim.api.nvim_exec2(ctx.args, { output = true })
+  local lines = vim.split(vim.api.nvim_exec2(ctx.args, { output = true }).output, "\n", { plain = true })
+
   vim.cmd('new')
   vim.api.nvim_buf_set_lines(0, 0, -1, false, lines)
   vim.opt_local.modified = false
