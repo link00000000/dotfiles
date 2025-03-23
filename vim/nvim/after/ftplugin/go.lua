adcc2218d28e7a0225cd6ad7c3835a6484d9b113
link00000000 <crandall.logan@gmail.com>
2025-03-23 11:12:25 -0400

    Use filter function for go format on save

--- vim/nvim/after/ftplugin/go.lua
+++ vim/nvim/after/ftplugin/go.lua
@@ -3,20 +3,8 @@ vim.api.nvim_create_augroup("GoFmtOnSave", { clear = true })
 vim.api.nvim_create_autocmd("BufWritePre", {
     group = "GoFmtOnSave",
     pattern = { "*.go" },
-    callback = function()
-        local gopls_attached = false
-        for i, client in ipairs(vim.lsp.buf_get_clients()) do
-          if client.name == "gopls" then
-            gopls_attached = true 
-            break
-          end
-        end
-
-        if gopls_attached == false then
-          return
-        end
-
-        vim.lsp.buf.format()
+    callback = function ()
+        vim.lsp.buf.format({ filter = function (client) return client.name == "gopls" end })
     end,
 })
 
